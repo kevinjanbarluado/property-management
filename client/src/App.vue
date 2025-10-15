71bd49cf27ac55e1874b93689952ff20ca533798
Kevin Jan Barluado <kevinjanbarluado2@gmail.com>
2025-10-15 21:36:57 +0800

    feat: integrate agent management into main app

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,9 @@
-<script setup></script>
+<script setup>
+import AgentManagement from './views/AgentManagement.vue'
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <AgentManagement />
 </template>
 
 <style scoped></style>
